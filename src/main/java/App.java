1bb2de5af90c131bb15f5c6e4cf1ed3b9c3882b9
GuiDevFatec <guilherme.santos417@fatec.sp.gov.br>
2025-10-02 20:01:22 -0300

    Resolução Atividade 4
    Guilherme Brito dos Santos

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,30 +1,60 @@
-package main.java;
+//package main.java; -> Tirei essa linha porque estava dando erro no meu computador, não estava conseguindo acessar a classe "CorpoHumano"
 import java.util.Scanner;
 
 public class App {
-        //Comentário
-
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
+    public static void main(String[] args) throws Exception {
+    //Biblioteca para entrada de dados
         Scanner ler = new Scanner(System.in); 
 
-        //variável local a
-        String v2;
-        int v1;
+        double massa, volume, densidade, altura, imc;
 
-        System.out.println("Comando de Saida de Dados");
+    //Entrada dos dados do usuário
+        System.out.println("=== CRIAÇÃO DO OBJETO CORPO HUMANO ===");
+        System.out.printf("Digite a massa inicial(kg): ");
+        massa = ler.nextDouble();
         
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
+        System.out.printf("Digite o volume inicial(m³): ");
+        volume = ler.nextDouble();
+        
+        System.out.printf("Digite a densidade inicial(kg/m³): ");
+        densidade = ler.nextDouble();
+        
+        System.out.printf("Digite a altura inicial(m): ");
+        altura = ler.nextDouble();
 
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
+    //Iniciando valores de entrada em um novo objeto
+        CorpoHumano c1 = new CorpoHumano(massa, volume, densidade, altura);
+        c1.massa = "2"; 
 
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
+    //Calculando imc
+        imc = c1.calcularIMC();
 
+    //Saída de dados    
+        System.out.println("\n=== VALORES INICIAIS ===");
+        System.out.println("Massa: " + c1.getMassa() + "kg");
+        System.out.println("Volume: " + c1.getVolume() + "m³");
+        System.out.println("Densidade: " + c1.getDensidade() + "kg/m³");
+        System.out.println("Altura: " + c1.getAltura() + "m");
+        System.out.println("IMC: " + imc + "\n");
+    
+    //Segunda entrada de dados
+        System.out.printf("Digite uma nova massa(kg): ");
+        massa = ler.nextDouble();
+        
+        System.out.printf("Digite uma nova altura(m): ");
+        altura = ler.nextDouble();
+    
+    //Usando os setter para atribuir valor aos atributos do objeto c1
+        c1.setMassa(massa);
+        c1.setAltura(altura);
 
+    //Calculando imc novamente
+        imc = c1.calcularIMC();
+
+    //Segunda saída de dados
+        System.out.println("\n=== NOVOS VALORES ===");
+        System.out.println("Massa: " + c1.getMassa());
+        System.out.println("Altura: " + c1.getAltura());
+        System.out.println("IMC: " + imc + "\n");
     }
 }
--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,55 @@
-package main.java;
-
+//package main.java;
 public class CorpoHumano {
+    private double volume, densidade, altura;
+    
+    public double massa;
 
+//Construtor da classe
+    public CorpoHumano(double massa, double volume, double densidade, double altura){
+        this.massa = massa;
+        this.volume = volume;
+        this.densidade = densidade;
+        this.altura = altura;
+    }
+
+//Calcular índice de massa corporal(IMC)
+    public double calcularIMC(){
+        return this.massa / (this.altura * this.altura);
+    }
+
+//Métodos setters
+    public void setMassa(double massa) {
+        this.massa = massa;
+    }
+
+//    private void setVolume(double volume)
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public void setDensidade(double densidade) {
+        this.densidade = densidade;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+
+//Métodos getters
+    public double getMassa() {
+        return this.massa;
+    }
+
+    public double getVolume() {
+        return this.volume;
+    }
+
+    public double getDensidade() {
+        return this.densidade;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
 }
